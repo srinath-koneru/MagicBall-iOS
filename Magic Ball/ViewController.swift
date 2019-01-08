07ef718bf18163dc70220a10dda1bb09f40f6157
Srinath Koneru <srinath.koneru@centurylink.com>
2019-01-08 14:11:24 -0600

    initial commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Srinath Koneru on 1/8/19.
+//  Copyright © 2019 Srinath Koneru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
