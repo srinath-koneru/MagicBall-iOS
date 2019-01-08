da00f2f8317d59115ba671ce0c1099eb5fd88f1d
Srinath Koneru <srinath.koneru@centurylink.com>
2019-01-08 15:26:03 -0600

    more changes

--- Magic Ball/ViewController.swift
+++ Magic Ball/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MagicBall: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MagicBall.image = UIImage(named: getAnswerBallImage())
     }
 
-
+    @IBAction func AskButton(_ sender: Any) {
+        MagicBall.image = UIImage(named: getAnswerBallImage())
+    }
+    
+    func getAnswerBallImage() -> String {
+        let randonNumber = Int.random(in: 0...4)
+        let imageArr = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+        return imageArr[randonNumber]
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        MagicBall.image = UIImage(named: getAnswerBallImage())
+    }
 }
 
